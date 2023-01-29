97cce93d69d0612a525055f65d2c05f0d972c47a
Md Sajibur Rahman <83011864+GameDevSajib@users.noreply.github.com>
2023-01-29 21:40:07 +0600

    Update practice4.sh

--- practice4.sh
+++ practice4.sh
@@ -1,1 +1,16 @@
 
+
+
+
+###using thrd bracket
+word=abcd
+if [[ $word -eq  "abcd" ]]
+ then
+      echo "yes!! they are equal"
+
+elif  [[ $word == "sajib" ]]
+then
+       echo  "condition is false"
+else
+        echo  " i am sorry!!!"
+fi
