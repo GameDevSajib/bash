352c50434b6e1eb05aca5f4e62bbdf3f6d0d6dd6
Md Sajibur Rahman <83011864+GameDevSajib@users.noreply.github.com>
2023-01-29 21:19:06 +0600

    Update practice2.sh

--- practice2.sh
+++ practice2.sh
@@ -1,1 +1,23 @@
 
+#!/bin/bash
+
+echo "BISMILLAH"
+echo  $BASH
+echo   $BASH_VERSION
+
+name=sajib
+echo my name is $name
+
+read -p "username:" user
+read  -sp "password" pass
+echo "username is : $user"
+echo  "password is :$pass"
+
+
+###NOW GETTING STARTED WITH CONDITION
+
+count=10
+if(($count>9))
+   then
+   echo "condition is true"
+fi
