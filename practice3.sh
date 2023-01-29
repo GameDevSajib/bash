8fbf6f0649a49b5777639d1df552e737a95cc8b7
Md Sajibur Rahman <83011864+GameDevSajib@users.noreply.github.com>
2023-01-29 21:33:08 +0600

    Update practice3.sh

--- practice3.sh
+++ practice3.sh
@@ -1,1 +1,25 @@
+  GNU nano 6.4                                          practice2.sh                                                    echo my name is $name
+
+read -p "username:" user
+read  -sp "password" pass
+echo "username is : $user"
+echo  "password is :$pass"
+
+
+###NOW GETTING STARTED WITH CONDITION
+
+count=10
+if(($count>9))
+   then
+   echo "condition is true"
+fi
+
+
+
+###using third bracket
+word=abcd
+if [[ $word -eq  "abcd" ]]
+ then
+      echo "yes!! they are equal"
+fi
 
