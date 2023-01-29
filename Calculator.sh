9b076eef7080387a826252ef8bbd4bc350db6a8c
Md Sajibur Rahman <83011864+GameDevSajib@users.noreply.github.com>
2023-01-29 22:34:13 +0600

    Update Calculator.sh

--- Calculator.sh
+++ Calculator.sh
@@ -1,1 +1,14 @@
 
+
+############### MAKING A CALCULATOR  ################
+
+read  a
+read  b
+
+echo "jog :  $(($a+$b))"
+echo
+echo biog :  $(($a-$b))
+echo
+echo  gun :   $(($a*$b))
+echo
+echo  vag :    $(($a/$b))
