9d6ccc0d84a83a039fee3ac3887879e709ffb433
Md Sajibur Rahman <83011864+GameDevSajib@users.noreply.github.com>
2023-01-30 15:48:47 +0600

    Update string.sh

--- string.sh
+++ string.sh
@@ -1,1 +1,11 @@
-
+        ###string##
+        
+        read st1
+        read st2
+        if [ $st1 == $st2 ]
+         then
+              echo "thay are equal"
+              else
+                   echo "thay are not equal"
+                   fi
+              
