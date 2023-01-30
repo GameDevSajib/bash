c098a1cff08619af09a9d15f002b0eec10df4948
Md Sajibur Rahman <83011864+GameDevSajib@users.noreply.github.com>
2023-01-30 15:22:20 +0600

    Update Loop_With_Condition.sh

--- Loop_With_Condition.sh
+++ Loop_With_Condition.sh
@@ -1,1 +1,14 @@
 
+   echo "loop with condition"       
+    for((i=0;i<=10;i++))
+        do
+              if [ $i -gt 5 ]
+                    then
+                          break
+              fi
+                          echo $i        
+        
+        
+        
+        done
+          
