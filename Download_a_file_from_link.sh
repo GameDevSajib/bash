95a88d2beeef7fb3b1921a1e0108b8dd9dc2673d
Md Sajibur Rahman <83011864+GameDevSajib@users.noreply.github.com>
2023-01-31 17:30:26 +0600

    Create Download_a_file_from_link.sh

---
+++ Download_a_file_from_link.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ url="https://images.unsplash.com/photo-1560114928-40f1f1eb26a0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y3V0ZSUyMGFuaW1hbHxlbnwwfHwwfHw%3D&w=1000&q=80"
+
+curl ${url} -O
